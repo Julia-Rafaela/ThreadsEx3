18ec2695618d6a7489630a2249a737084d5c031b
Julia-Rafaela <124639320+Julia-Rafaela@users.noreply.github.com>
2023-03-08 22:34:10 -0300

    atualizado

--- src/controller/CorridaDeSapos.java
+++ src/controller/CorridaDeSapos.java
@@ -1,19 +1,30 @@
 package controller;
 
+//3) Fazer uma aplicação de uma corrida de sapos, com 5
+//Threads, cada Thread controlando 1 sapo. Deve haver um
+//tamanho máximo para cada pulo do sapo (em metros) e a
+//distância máxima para que os sapos percorram. A cada salto,
+//um sapo pode dar uma salto de 0 até o tamanho máximo do
+//salto (valor aleatório). Após dar um salto, a Thread, para cada
+//sapo, deve mostrar no console, qual foi o tamanho do salto e
+//quanto o sapo percorreu. Assim que o sapo percorrer a
+//distância máxima, a Thread deve apresentar que o sapo
+//chegou e qual sua colocação.
+
 public class CorridaDeSapos  extends Thread{
 	
-	private int numero = 1;
+	public static int numero=1;
 	
 	public  CorridaDeSapos () {
-		this.numero = numero;
+		
 	}
  
 	@Override
 	public void run() {
 		
 		corrida();
-	System.out.println("-"+ getId()+" " +numero+ "° lugar");
-	numero = numero+1;
+	System.out.println("Sapo# "+getId()+" " +numero+ "° lugar");
+	numero++;
 	}
 
 	private void corrida() {
